57fd650e32e6f7fd0e910f2898fd77cebc56f294
var787 <hegde.rakshan@gmail.com>
2020-11-14 23:56:00 -0600

    commit tree 8f8cf4a7cf8c7e29d42c966c46061495c43fac5c

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/name' do
+        "My name is Rakshan"
+      end
+    
+    get '/hometown' do
+        "My hometown is Chicago"
+        end
+
+    get '/favorite-song' do
+         "My favorite song is Black by Pearl Jam"
+        end
+
 end
